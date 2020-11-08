e44ffb1f161df6a1db42cd7ee7d21f84a3688650
Tasfia Islam <tasf1a027@gmail.com>
2020-11-09 05:09:33 +0600

    Test Login Registration implemented

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from authentication.views import login, register
+from authentication.views import login_page, register_page
 
 urlpatterns = [
-    path('login/', login),
-    path('register/', register),
+    path('login/', login_page, name="login"),
+    path('register/', register_page, name="register"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,12 +1,43 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+
+# To show flash messages
+from django.contrib import messages
 
 # LOGIN VIEW ENDPOINT
 
-def login(request):
-    return render(request, 'login.html')
+def login_page(request):
+
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        # Check if user exists in database
+        if user is not None:
+            login(request, user)
+            return redirect('/posts/')
+        else:
+            messages.info(request, 'Username or Password is incorrect')
+
+    context = {}
+    return render(request, 'test_login.html', context)
 
 
-def register(request):
-    return render(request, 'register.html')
+def register_page(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')           
+            messages.success(request, 'Account was created for '+username)
+            return redirect('login')
+
+    context = {'form':form}
+    return render(request, 'test_register.html', context)
