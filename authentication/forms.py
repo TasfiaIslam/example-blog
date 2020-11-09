009dc6c43e0b2fe2e1bbb18205a95a132b2b5b80
Tasfia Islam <tasf1a027@gmail.com>
2020-11-09 14:06:30 +0600

    Email, firstname, lastname validations added

--- authentication/forms.py
+++ authentication/forms.py
@@ -3,8 +3,39 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+
 
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','first_name','last_name', 'email', 'password1', 'password2']
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        qs = User.objects.filter(email=email)
+        if qs.exists():
+            raise forms.ValidationError("email is taken")
+        return email
+    
+    def email_validation(self):
+        email = self.cleaned_data.get('email')
+        qs = User.objects.filter(email=email)
+        try:
+            validate_email(qs)
+            return True
+        except ValidationError:
+            return False
+
+    # Make first_name a required field
+    def clean_first_name(self):
+        if self.cleaned_data["first_name"].strip() == '':
+            raise ValidationError("First name is required.")
+        return self.cleaned_data["first_name"]
+
+    # Make last_name a required field
+    def clean_last_name(self):
+        if self.cleaned_data["last_name"].strip() == '':
+            raise ValidationError("Last name is required.")
+        return self.cleaned_data["last_name"]
