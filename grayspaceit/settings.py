a37c370d95aa2528ac5353527d292723e4c5cb6c
Tasfia Islam <tasf1a027@gmail.com>
2020-11-11 23:47:02 +0600

    Tweaked app for heroku

--- grayspaceit/settings.py
+++ grayspaceit/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -57,6 +61,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware', #Whitenoise middleware
 ]
 
 ROOT_URLCONF = 'grayspaceit.urls'
@@ -132,9 +138,15 @@ STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [str(BASE_DIR / "static")]
 
+# for whitenoise storage heroku
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+#jwt token with rest framework
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+django_heroku.settings(locals())
 
